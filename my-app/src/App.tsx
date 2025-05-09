21c7a47c7d3e4c5ff3f4b61a0143ffbaf6dd6e28
Sid <aishwaryakumari@Aishwaryas-MacBook-Pro.local>
2025-05-09 15:41:55 +0530

    react code added

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ascendons from "./ascendons.jpeg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="" target="_blank">
+          <img src={ascendons} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
