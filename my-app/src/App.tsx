dea0c7cd3a599fd48af374c089c649eef05601e3
Naman Jha <108355610+mrjha4050@users.noreply.github.com>
2025-05-10 18:32:48 +0530

    navbar alignment done and particlejs json uploaded

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Services from "./pages/Services"; 
 import Contact from "./pages/Contact"; 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
