a34a37a59b5c7222363b72f6bd00ead5b5fdbf47
Naman Jha <108355610+mrjha4050@users.noreply.github.com>
2025-05-11 17:46:50 +0530

    setup particle js

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,6 +1,26 @@
+import ParticlesComponent from "../components/Particle";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="relative h-screen w-full overflow-hidden">
+        {/* Particles Background */}
+        <ParticlesComponent id="tsparticles" className="absolute inset-0" />
+
+        <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-4">
+          <h1 className=" text-white/90 mb-12 max-w-3xl">
+            Smart, Scalable, and Stunning Software Solutions.
+          </h1>
+
+           
+        </div>
+      </section>
+
+      <section>
+      
+      </section>
+    </>
+  );
+};
+
+export default Home;
