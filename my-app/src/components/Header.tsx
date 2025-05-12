7a29e0c8f1f20d9ffa650108d0b402a69dd3a4bc
Sid <aishwaryakumari@Aishwaryas-MacBook-Pro.local>
2025-05-12 19:40:11 +0530

    Added logo on title

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../ascendons.jpeg";
+import logo from "../../public/ascendons.png";
 import "./Header.css";
 
 const Header = () => {
