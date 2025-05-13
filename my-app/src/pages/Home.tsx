c8204a58af32768a846d3ec4af309ae8cb4fcdb4
Naman Jha <108355610+mrjha4050@users.noreply.github.com>
2025-05-13 23:27:23 +0530

    particles js and padding is done

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,17 +1,26 @@
+import ParticlesComponent from "../components/Particle.tsx";
+import React from "react";
+
 const Home: React.FC = () => {
   return (
     <>
       <section className="relative h-screen w-full overflow-hidden">
-        {/* Particles Background */}
-
-        <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-4">
+          <ParticlesComponent id="tsparticles" className="absolute inset-0" />
+          <div className="ative z-10 h-full flex flex-col justify-center items-center text-center px-4">
           <h1 className=" text-white/90 mb-12 max-w-3xl">
-            Smart, Scalable, and Stunning Software Solutions.
+            Smart, Scalable, and Stunning Software Solutions
           </h1>
         </div>
       </section>
 
-      <section></section>
+        <section className="relative h-screen w-full overflow-hidden bg-white">
+            <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-4">
+                <h1 className=" text-black/90 mb-12 max-w-3xl">
+                   Our Services
+                </h1>
+            </div>
+        </section>
+
     </>
   );
 };
