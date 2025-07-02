fa450c0774ab30738d4b8ea3a6dd652f8eeeab9c
Sid <aishwaryakumari@Aishwaryas-MacBook-Pro.local>
2025-07-03 04:09:52 +0530

    Fixed errors

---
+++ my-app/src/components/GenericAccordionContent.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+
+interface GenericAccordionContentProps {
+  title: string;
+  onNext: () => void;
+  onPrevious: () => void;
+}
+
+const GenericAccordionContent: React.FC<GenericAccordionContentProps> = ({ title, onNext, onPrevious }) => (
+  <>
+    <Typography>{title} content goes here.</Typography>
+    <Box display="flex" justifyContent="space-between" mt={3}>
+      <Button variant="outlined" onClick={onPrevious}>Previous</Button>
+      <Button variant="contained" onClick={onNext}>Next</Button>
+    </Box>
+  </>
+);
+
+export default GenericAccordionContent;
