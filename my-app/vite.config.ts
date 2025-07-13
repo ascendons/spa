87a30adfa3fd3558c19a8277e8ab97e27cfd3d85
d24sid <siddharthagarwal65@gmail.com>
2025-07-13 14:15:19 +0530

    Sid new (#60)
    * Fixed errors
    
    * Update QRGenerator.tsx
    
    fixed merge error
    
    * Fixed lint
    
    * Added formatting
    
    * url updated
    
    * Changes in Footer
    
    * Update deploy.yml
    
    * Update main.yml
    
    * Update package.json
    
    * added format
    
    * removed format
    
    * Update vite.config.ts
    
    ---------
    
    Co-authored-by: Sid <aishwaryakumari@Aishwaryas-MacBook-Pro.local>

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/spa/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ["@tsparticles/slim"],
